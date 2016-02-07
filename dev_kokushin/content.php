85fbfd15a9b7430a56292ed24a86ec08f38acbd3
Yusuke Ishiguro <ishiguro@un-t.com>
2016-02-07 19:57:36 +0900

    <time>タグを修正した

--- dev_kokushin/content.php
+++ dev_kokushin/content.php
@@ -8,7 +8,7 @@
   <div class="meta">
     <div class="category"><?php echo $cat_name; ?></div>
     <h2><span><?php the_title(); ?></span></h2>
-    <time><?php the_time('Y.m.d');?></time>
+    <time datetime="<?php the_time('Y-m-d H:i:s'); ?>"><?php the_time('Y.m.d'); ?></time>
   </div>
 </a>
 </article>
